fdd274cc5716491f0c4a7979f5408aa9b5de4ea0
TimNape <ntimothys@gmail.com>
2020-05-01 07:54:00 +0200

    LinkedIn graphql essential training

---
+++ linkedin_graphql_essential_training/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing!');
+});
+
+//const root = {hello: () => "Hi, I'm Tim"}; //root resolve
+const root = {friend: () => {
+    return {
+        "id": 342342,
+        "firstName": "John",
+        "lastName": "Due",
+        "gender": "Male",
+        "language": "English",
+        "email": "john@example.com"
+    }
+}};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+
+}));
+
+app.listen(8080, () => console.log('Running server o port localhost:8080'));
